e6bda7cf40146fced0e2ccd1d057dc082cbf66b6
nok-chan <HrithikSarkar45@protonmail.com>
2022-06-28 15:27:09 +0530

    in the middle of generating the random password

--- app.js
+++ app.js
@@ -1,8 +1,66 @@
-const pw = document.getElementBy("pw");
-const copy = document.getElementBy("copy");
-const passLength = document.getElementBy("pass-length");
-const upper = document.getElementBy("upper");
-const lower = document.getElementBy("lower");
-const number = document.getElementBy("number");
-const symbol = document.getElementBy("symbol");
-const generate = document.getElementBy("generate");
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const passLengthEl = document.getElementById("pass-length");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!#$%^&*()_+";
+
+console.log(passLengthEl.options[passLengthEl.selectedIndex].text);
+
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+function generatePassword() {
+
+    const passLength = passLengthEl.options[passLengthEl.selectedIndex].text;
+    let password = "";
+    for(let i = 0; i < passLength; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+}
+
+function generateX(){
+
+    const xs = [];
+
+    if(upperEl.checked) {
+        xs.push(getUppercase());
+    }
+    if(lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+    if(numberEl.checked) {
+        xs.push(getNumber());
+    }
+    if(symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePassword);
